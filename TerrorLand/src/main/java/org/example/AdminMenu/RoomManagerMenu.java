b902e1b0a679fc6e50787724d01b008e3c2e5e5b
santiago <santiagoso1998@gmail.com>
2024-10-25 12:48:59 +0200

    Calculate Stock Value Function

--- TerrorLand/src/main/java/org/example/AdminMenu/RoomManagerMenu.java
+++ TerrorLand/src/main/java/org/example/AdminMenu/RoomManagerMenu.java
@@ -2,6 +2,7 @@ package org.example.AdminMenu;
 
 import org.example.dao.DisplayDao;
 import org.example.dao.RoomDao;
+import org.example.dao.StockDao;
 import org.example.util.MenuHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,25 +12,28 @@ public class RoomManagerMenu {
     private static final DisplayDao DISPLAY = new DisplayDao();
     private static final ElementCreator CREATOR = new ElementCreator();
     private static final RoomDao ROOM_DAO = new RoomDao();
+    private static final StockDao STOCK_DAO = new StockDao();
 
     public void manageRooms(){
         int op = 0;
         do{
             op = MenuHelper.readSelection("Choose an option: ", ">",
                     "1. Display Rooms",
-                            "2. Display Active Room Elements",
+                            "2. Display Elements",
                             "3. Create Room",
                             "4. Delete Room",
                             "5. Retrieve Deleted Room",
-                            "6. Return to Admin Menu");
+                            "6. Show Inventory Value",
+                            "7. Return to Admin Menu");
             switch (op){
                 case 1 -> DISPLAY.displayActiveRooms();
                 case 2 -> DISPLAY.displayActiveRoomElements();
                 case 3 -> CREATOR.createRoomHasElements();
                 case 4 -> ROOM_DAO.deleteRoom();
                 case 5 -> ROOM_DAO.retrieveRoom();
-                case 6 -> LOGGER.info("Returning to the Admin Menu");
+                case 6 -> STOCK_DAO.displayTotalPriceOfAllElementsInRooms();
+                case 7 -> LOGGER.info("Returning to the Admin Menu");
             }
-        } while(op != 6);
+        } while(op != 7);
     }
 }
---
+++ TerrorLand/src/main/java/org/example/dao/StockDao.java
@@ -0,0 +1,32 @@
+package org.example.dao;
+
+import org.example.AdminMenu.RoomManagerMenu;
+import org.example.factory.StockFactory;
+import org.example.util.OutputBuilder;
+import org.example.util.Queries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import static org.example.mysql.MySqlHelper.getConnection;
+
+public class StockDao implements StockFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockDao.class);
+    private static final OutputBuilder OUTPUT_BUILDER = new OutputBuilder();
+
+    public void displayTotalPriceOfAllElementsInRooms() {
+        String query = Queries.buildCalculateStockValue(); // Ensure this retrieves the query above
+        try (Connection conn = getConnection("escape_room");
+             PreparedStatement psmt = conn.prepareStatement(query);
+             ResultSet rs = psmt.executeQuery()) {
+            if (rs.next()) {
+                String totalPrice = OUTPUT_BUILDER.buildTotalPriceOutput(rs);
+                System.out.println(totalPrice);
+            }
+        } catch (SQLException e) {
+            LOGGER.error("Could not retrieve the total price for all elements in rooms: {}", e.getMessage());
+        }
+    }
+}
